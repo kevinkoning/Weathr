3b0cc30af4e7d9f6d2e97437afcfea1450190d24
Kevin <kevinkoning99@gmail.com>
2026-02-08 17:06:36 -0800

    Initial Commit

---
+++ Weathr/WeathrApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeathrApp.swift
+//  Weathr
+//
+//  Created by Kief on 08/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct WeathrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
